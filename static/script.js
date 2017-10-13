5ef3d1be9396296acc5fbeee665f2d740d0af3b9
Bram <bramdenkers98@gmail.com>
2017-10-13 21:10:22 +0200

    Initial Commit

---
+++ static/script.js
@@ -0,0 +1,35 @@
+ocument.getElementById("camera").onclick = callPython;
+document.getElementById("trash").onclick = callPython;
+document.getElementById("edit").onclick = callPython;
+document.getElementById("erase").onclick = callPython;
+document.getElementById("undo").onclick = callPython;
+document.getElementById("redo").onclick = callPython;
+document.getElementById("start").onclick = callPython;
+
+
+function callPython(){
+	console.log(this.id);
+
+
+	if (this.id == "camera"){
+		document.getElementById("maze").src = "steam.mjpg";
+	}
+
+	if (this.id == "start"){
+		document.getElementById("maze").src = "https://camo.githubusercontent.com/fe94d9aba32c8683e6f5acfeddcf153577fd8051/687474703a2f2f6e756c6c70726f6772616d2e636f6d2f696d672f706174682f6d617a652e676966";
+	    //$("#Pimage").show();
+	}
+
+	if (this.id == "edit"){
+		document.getElementById("maze").src = "/static/image.jpg";
+
+	}
+
+	var dest = '/';
+	dest += this.id;
+
+	$.ajax({
+		type: "GET",
+		url: dest,
+	});
+}
