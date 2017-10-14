4b940873e947e98fe8256dd2d5169948cadf7db4
Bram <bramdenkers98@gmail.com>
2017-10-14 11:52:26 +0200

    Livestream feed

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, render_template
+import gen as gen
+from flask import Flask, render_template, Response
+from camera import Camera
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
-@app.route('/test')
-def start():
     return render_template('index.html')
+
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 
 
