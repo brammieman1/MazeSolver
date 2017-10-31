5029672d189b86e6f4f58e658b22e886dd9e2d5a
Heleen <h.a.visserman@student.utwente.nl>
2017-10-31 09:55:17 +0000

    picture.py IS WORKINGgit add . <~ (party popper)

--- picture.py
+++ picture.py
@@ -1,15 +1,26 @@
+from io import BytesIO
 import time
 import picamera
 import numpy as np
 import BlackWhite2
+from PIL import Image
+
+output = 'output.jpg'
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (320,240)
+  #  stream = BytesIO()
+   # camera.start_preview()
+   # time.sleep(2)
+ #   camera.capture(stream, format='jpeg')
+    # "Rewind" the stream to the beginning so we can read its content
+  #  stream.seek(0)
+ #   image = Image.open(stream)
+ #   camera.resolution = (320,240)
     camera.framerate = 24
     time.sleep(2)
-#    output = np.empty((240,320, 3), dtype = np.uint8)
-    camera.color_effects = (128,128)
-    camera.capture(output, 'rgb')
-    print(output)
-    outputnew = BlackWhite2.binarize_array(output, 75)
+  #  output = np.empty((240,320, 3), dtype = np.uint8)
+  #  camera.color_effects = (128,128)
+    camera.capture(output)
+    print('picture taken')
+    outputnew = BlackWhite2.binarize_image(output, 75)
     print(outputnew)
