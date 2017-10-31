93a10e8a51a63ee881fa73849f1c09b9fd18ba81
Bram <a.j.denkers@student.utwente.nl>
2017-10-31 09:57:27 +0100

    spi.py fix

--- spi.py
+++ spi.py
@@ -1,17 +1,19 @@
 import time
 import wiringpi
-SPIchannel = 0 #SPI Channel (CE0)
-SPIspeed = 10 #Clock Speed in Hz
+
+SPIchannel = 0  # SPI Channel (CE0)
+SPIspeed = 10  # Clock Speed in Hz
 wiringpi.wiringPiSetupGpio()
 wiringpi.wiringPiSPISetup(SPIchannel, SPIspeed)
 
 count = 0
 
 while (True):
-	sendData = "abcdefghijklmnopqrstuvw"  #will send ONE bytes, a byte 1
-	recvData = wiringpi.wiringPiSPIDataRW(SPIchannel, sendData)
-	count = count + 1
-	time.sleep(1)
-	print(count)
-	print('receiveddata =', recvData[1])
+    prefix = chr(63)   # 63 is send something back. 85 is only send.
+    sendData = chr(85)  # will send ONE bytes, a byte 1
+    recvData = wiringpi.wiringPiSPIDataRW(SPIchannel, prefix + sendData)
+    count = count + 1
+    time.sleep(1)
+    print(count)
+    print('receiveddata =', recvData[1])
 
