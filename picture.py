26b03598d9250892bc867285e30e2d91773b1e1d
Heleen <h.a.visserman@student.utwente.nl>
2017-10-31 10:19:49 +0000

    Gave picture.py a method and made it executable with a main function

--- picture.py
+++ picture.py
@@ -7,20 +7,16 @@ from PIL import Image
 
 output = 'output.jpg'
 
-with picamera.PiCamera() as camera:
-  #  stream = BytesIO()
-   # camera.start_preview()
-   # time.sleep(2)
- #   camera.capture(stream, format='jpeg')
-    # "Rewind" the stream to the beginning so we can read its content
-  #  stream.seek(0)
- #   image = Image.open(stream)
- #   camera.resolution = (320,240)
-    camera.framerate = 24
-    time.sleep(2)
-  #  output = np.empty((240,320, 3), dtype = np.uint8)
-  #  camera.color_effects = (128,128)
-    camera.capture(output)
-    print('picture taken')
-    outputnew = BlackWhite2.binarize_image(output, 75)
-    print(outputnew)
+def getArray():
+    with picamera.PiCamera() as camera:
+       camera.framerate = 24
+       time.sleep(2)
+       camera.capture(output)
+       print('picture taken')
+       outputnew = BlackWhite2.binarize_image(output, 75)
+       print(outputnew)
+       return outputnew
+
+if __name__ == "__main__":
+    getArray()
+
