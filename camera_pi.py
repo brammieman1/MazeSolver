cace1c07d0e1b43296c7758d708ece15cd098f73
Bram <bramdenkers98@gmail.com>
2017-11-06 11:54:58 +0100

    rescale resolution

--- camera_pi.py
+++ camera_pi.py
@@ -9,7 +9,7 @@ class Camera(BaseCamera):
     @staticmethod
     def frames():
         with picamera.PiCamera() as camera:
-            camera.resolution = (320, 240)
+            camera.resolution = (640, 480)
             # let camera warm up
             time.sleep(2)
 
--- functions.py
+++ functions.py
@@ -16,7 +16,7 @@ def picture():
    time.sleep(1)
    with picamera.PiCamera() as camera:
        camera.framerate = 24
-       camera.resolution = (320, 240)
+       camera.resolution = (640, 480)
        time.sleep(2)
        camera.capture(output)
        print('picture taken')
