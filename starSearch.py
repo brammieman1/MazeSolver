58e27d3bcb7f16818aa7427e53dcb2f0f368162d
Bram <bramdenkers98@gmail.com>
2017-11-06 09:25:10 +0100

    starSearch worked

--- starSearch.py
+++ starSearch.py
@@ -3,9 +3,12 @@ import sys
 
 import numpy as np
 
+#maze = np.array([[]])
+
 
 def AStar(start, goal, neighbor_nodes, dist_between, heuristic_cost_estimate):
-    neighbor_nodes =
+
+
 
     def reconstruct_path(came_from, current_node):
         path = [current_node]
@@ -46,39 +49,35 @@ def AStar(start, goal, neighbor_nodes, dist_between, heuristic_cost_estimate):
                     openset.add(neighbor)
     print("no path found :(")
 
-def is_blocked(p,maze):
+def is_blocked(p):
     x,y = p
     if maze[x,y] == 1:
         return True
 
-def von_neumann_neighbors(p, maze):
-    x, y = p
+def von_neumann_neighbors(p):
+    x,y = p
+
     neighbors = [(x-1, y), (x, y-1), (x+1, y), (x, y+1)]
-    return [p for p in neighbors if not is_blocked(p, maze)]
+    return [p for p in neighbors if not is_blocked(p)]
 def manhattan(p1, p2):
     return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1])
 def squared_euclidean(p1, p2):
     return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
 
-if __name__ == '__main__':
-    start = (1, 1)
-    goal = (3, 3)
-
-# invoke: python mazesolver.py <mazefile> <outputfile>[.jpg|.png|etc.]
-
-    maze = np.array([[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 1, 0, 1, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]])
-
-    path = AStar(start,
-                 goal,
-                 von_neumann_neighbors,
-                 manhattan,
-                 manhattan,
-             #lambda p1,p2 : 4*manhattan(p1,p2),
-             #squared_euclidean,
-             )
-
+def ready(test, start, end):
+    start = start
+    goal = end
+    global maze
+    maze = test
+    path = AStar(start,goal,von_neumann_neighbors, manhattan,manhattan)
     for position in path:
         x,y = position
-        maze[x,y] = 3 # red
+        maze[x,y] = 3 #red
+    print(maze)
+    return maze
 
-        print(maze)
+if __name__ == '__main__':
+    puzzle = np.array([[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 1, 0, 1, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]])
+    start = (1,1)
+    goal = (3,3)
+    ready(puzzle,start, goal)
